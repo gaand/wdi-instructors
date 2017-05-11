eb89e433441f1644e98556189bed2adbd76fcb8b
Antony Donovan <antony.donovan@generalassemb.ly>
2017-05-11 15:01:00 -0400

    Reupdate from node-template; conform to linter
    Also, add some tests

--- bin/pull-requests.js
+++ bin/pull-requests.js
@@ -4,103 +4,99 @@
   sub: true
 */
 
-'use strict';
+'use strict'
 
-const missingFromOtherList = require('../lib/missing-from-other-list');
+const missingFromOtherList = require('../lib/missing-from-other-list')
 
-require('dotenv').load();
+require('dotenv').load()
 
-const compare = (a, b) => a.github > b.github ? 1 : a.github < b.github ? -1 : 0;
+const compare = (a, b) => a.github > b.github ? 1 : a.github < b.github ? -1 : 0
 
 const pullsPromise = new Promise((resolve, reject) => {
+  const https = require('https')
 
-  let https = require('https');
+  const user = process.env.GHUSER
+  const token = process.env.GHTOKEN
+  const repo = process.argv[2]
 
-  let user = process.env.GHUSER;
-  let token = process.env.GHTOKEN;
-  let repo = process.argv[2];
-
-  let options = {
+  const options = {
     hostname: 'api.github.com',
     port: 443,
     path: `/repos/ga-wdi-boston/${repo}/pulls?state=all&per_page=100&sort=updated&direction=desc`,
     method: 'GET',
     auth: `${user}:${token}`,
     headers: {
-      'User-Agent': 'curl/7.38.0',
-    },
-  };
+      'User-Agent': 'curl/7.38.0'
+    }
+  }
 
   https.get(options, (res) => {
-    let result = '';
+    let result = ''
 
-    res.on('data', d => result += d);
+    res.on('data', d => { result += d })
 
     if (res.statusCode !== 200) {
-      //console.log('headers: ', res.headers);
-      res.on('end', () => reject(result));
+      // console.log('headers: ', res.headers);
+      res.on('end', () => reject(result))
     } else {
       res.on('end', () => {
-        let pulls = JSON.parse(result).map(p => ({
+        const pulls = JSON.parse(result).map(p => ({
           state: p.state,
-          github: p.user.login.toLowerCase(),
-        })).sort(compare);
-        let uniquePulls = [];
+          github: p.user.login.toLowerCase()
+        })).sort(compare)
+        const uniquePulls = []
         for (let i = 1; i < pulls.length; i++) {
           if (compare(pulls[i - 1], pulls[i])) {
-            uniquePulls.push(pulls[i - 1]);
+            uniquePulls.push(pulls[i - 1])
           }
         }
 
         if (pulls.length > 1 &&
-            compare(pulls[pulls.length - 1], pulls[pulls.length - 2]) ||
-            pulls.length) {
-          uniquePulls.push(pulls[pulls.length - 1]);
+            (compare(pulls[pulls.length - 1], pulls[pulls.length - 2]) ||
+            pulls.length)) {
+          uniquePulls.push(pulls[pulls.length - 1])
         }
 
-        resolve(uniquePulls);
-      });
+        resolve(uniquePulls)
+      })
     }
+  }).on('error', e => reject(e))
+})
 
-  }).on('error', e => reject(e));
+const developersPromise = new Promise((resolve, reject) => {
+  const developers = []
+  const fs = require('fs')
+  const parse = require('csv').parse
+  const parser = parse({
+    columns: h => h.map(c => c.toLowerCase())
+  })
 
-});
-
-let developersPromise = new Promise((resolve, reject) => {
-  let developers = [];
-  let fs = require('fs');
-  let parse = require('csv').parse;
-  let parser = parse({
-    columns: h => h.map(c => c.toLowerCase()),
-  });
-
-  let input = fs.createReadStream(process.env.DEVELOPERS);
-  input.on('error', e => reject(e));
+  const input = fs.createReadStream(process.env.DEVELOPERS)
+  input.on('error', e => reject(e))
 
   parser.on('readable', () => {
-    let record;
+    let record
     while ((record = parser.read())) {
-      record.github = record.github.toLowerCase();
-      developers.push(record);
+      record.github = record.github.toLowerCase()
+      developers.push(record)
     }
-  });
+  })
 
-  parser.on('error', e => reject(e));
-  parser.on('finish', () => resolve(developers));
-  input.pipe(parser);
-});
+  parser.on('error', e => reject(e))
+  parser.on('finish', () => resolve(developers))
+  input.pipe(parser)
+})
 
 Promise.all([pullsPromise, developersPromise]).then((results) => {
-  let missing = missingFromOtherList(results[0], results[1], compare);
+  const missing = missingFromOtherList(results[0], results[1], compare)
 
   console.log('closed pulls: ',
-    results[0].filter(p => p.state === 'closed').length);
-  console.log('pulls missing developers: ', missing[0].filter(p => p.state !== 'closed'));
-  console.log(`developers missing pulls (${missing[1].length}): `);
-  missing[1].forEach(d => console.log(d.given, d.family, `(${d.github})`));
-
+    results[0].filter(p => p.state === 'closed').length)
+  console.log('pulls missing developers: ', missing[0].filter(p => p.state !== 'closed'))
+  console.log(`developers missing pulls (${missing[1].length}): `)
+  missing[1].forEach(d => console.log(d.given, d.family, `(${d.github})`))
 }).catch(e => {
-  console.error('Something went wrong!');
-  console.error(e);
-  if (e.stack) console.error(e.stack);
-});
+  console.error('Something went wrong!')
+  console.error(e)
+  if (e.stack) console.error(e.stack)
+})
--- spec/missing-from-other-list.spec.js
+++ spec/missing-from-other-list.spec.js
@@ -1,14 +1,32 @@
-'use strict';
+'use strict'
 
-var missing = require('../lib/missing-from-other-list.js');
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+chai.use(chaiAsPromised)
+
+const expect = chai.expect
+
+const missing = require('../lib/missing-from-other-list.js')
+
+const a = [1, 2, 3, 5, 7, 11]
+const b = [0, 1, 2, 3, 5, 8, 13]
 
 describe('missingFromOtherList', () => {
-
-  describe('', () => {
-    it('is defined', () => {
-      expect(missing.toBeDefined());
-    });
-
-  });
-
-});
+  const pair = missing(a, b, (a, b) => a > b ? 1 : a < b ? -1 : 0)
+  describe('return value', function () {
+    it('is defined', function () {
+      expect(pair).to.be.defined
+    })
+  })
+  describe('in a, not in b', function () {
+    it('contains 7 and 11', function () {
+      expect(pair[0]).to.deep.equal([7, 11])
+    })
+  })
+  describe('in b, not in a', function () {
+    it('contains 0, 8, and 13', function () {
+      expect(pair[1]).to.deep.equal([0, 8, 13])
+    })
+  })
+})
